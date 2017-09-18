13f06c319712a7f2d79ad5ad9485b9bcb3a92a15
Thomas Haakon Townsend <thomas.haakon@gmail.com>
2017-09-18 17:09:47 +0100

    Refactor to use static methods for decorators remove logging add example of second class

--- src/example/index.ts
+++ src/example/index.ts
@@ -4,13 +4,32 @@ import * as express from "express";
 @Router.Application({port: 3000})
 class App {
 
-    @Router.route("/")
+    @Router.Route("/")
     public root(req: express.Request, res: express.Response, next: express.NextFunction) {
         res.send("foo");
     }
 
 
-    @Router.route(HttpMethods.GET, "/demo")
+    @Router.Route(HttpMethods.GET, "/demo")
+    public demo(req: express.Request, res: express.Response, next: express.NextFunction) {
+        res.send("bar");
+    }
+
+    public example() {
+        console.log("This is not a route");
+    }
+}
+
+@Router.Application({port: 3001})
+class App2 {
+
+    @Router.Route("/")
+    public root(req: express.Request, res: express.Response, next: express.NextFunction) {
+        res.send("foo");
+    }
+
+
+    @Router.Route(HttpMethods.GET, "/demo")
     public demo(req: express.Request, res: express.Response, next: express.NextFunction) {
         res.send("bar");
     }
--- src/index.ts
+++ src/index.ts
@@ -32,24 +32,36 @@ function getDefaultConfig(): IRouterConfig {
 
 export default class Router {
     private static routers: Map<string, Router> = new Map();
+    private static readonly ROUTER_ID = "expressRouterId";
 
     private readonly app = express();
     private cls: Object;
     private config: IRouterConfig;
 
-    public static readonly Application: (config: IRouterConfig) => ClassDecorator = config => {
-        const router = new Router();
-        router.config = { ...getDefaultConfig(), ...config };
+    private static getRouter(target: any): Router {
+        if (target[this.ROUTER_ID] === undefined) {
+            const id = v4();
+            Object.defineProperty(target, this.ROUTER_ID, { value: id, writable: false });
+            this.routers.set(id, new Router());
+        }
+        return this.routers.get(target[this.ROUTER_ID]);
+    }
+
+    public static Application(config: IRouterConfig): ClassDecorator {
         return (cls: any) => {
-            console.log(`has class: ${cls.name}`);
+            let router: Router = this.routers.get(cls.prototype[this.ROUTER_ID]);
+            if (!router) {
+                throw new Error("Class has no routes");
+            }
+            router.config = { ...getDefaultConfig(), ...config };
             router.cls = new cls();
             router.start();
         }
     }
 
-    public static route(path: string): MethodDecorator;
-    public static route(method: HttpMethods, path: string): MethodDecorator;
-    public static route(pathOrMethod: string, path?: string): MethodDecorator {
+    public static Route(path: string): MethodDecorator;
+    public static Route(method: HttpMethods, path: string): MethodDecorator;
+    public static Route(pathOrMethod: string, path?: string): MethodDecorator {
         let method: string;
         if (path === undefined) {
             path = pathOrMethod;
@@ -61,18 +73,14 @@ export default class Router {
             throw new Error("Incorrect method");
         }
         const id = v4();
-        console.log(`register path: ${path}, id: ${id}`);
         return (target, key, descriptor) => {
-            console.log(`Call path: ${path}, id: ${id}`);
-            console.log(target);
+            const router = this.getRouter(target);
             const value = (...args: any[]) => {
                 const fn: express.RequestHandler = descriptor.value as any;
                 if (args[0] === null) {
-                    console.log(`connecting path: ${path}, id: ${id}`);
-                    // this.app[method](path, (req, res, next) => {
-                    //     console.log(`triggering path: ${path}, id: ${id}`);
-                    //     return fn(req, res, next);
-                    // });
+                    router.app[method](path, (req, res, next) => {
+                        return fn(req, res, next);
+                    });
                 } else {
                     throw new Error("Router.start() was not called");
                 }
@@ -91,16 +99,13 @@ export default class Router {
         const objProto = Object.getPrototypeOf(this.cls);
         const keys = Object.getOwnPropertyNames(objProto).sort().filter((e, i, arr) => {
             let descriptor = Object.getOwnPropertyDescriptor(objProto, e);
-            console.log(descriptor);
             return (
                 (descriptor && descriptor.value["isRoute"]) &&
                 e != arr[i + 1] &&
                 typeof this.cls[e] == 'function' &&
                 e !== 'constructor') ? true : false;
         });;
-        console.log(keys);
         for (let key of keys) {
-            console.log(`starting key: ${key}`);
             this.cls[key](null);
         }
         this.app.listen(this.config.port, this.config.hostname);
